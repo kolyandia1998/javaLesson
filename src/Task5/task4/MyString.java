b2f18f7d351d067b33e5ebdc1f4a96d0cf8e3d63
kolyandia1998 <93881367+kolyandia1998@users.noreply.github.com>
2022-07-26 14:49:37 +0300

    test

--- src/Task5/task4/MyString.java
+++ src/Task5/task4/MyString.java
@@ -1,5 +1,65 @@
 package Task5.task4;
 
-public class MyString {
+import javax.xml.stream.events.Characters;
 
+public class MyString {
+    private final char[] chars;
+
+    public char[] getChars() {
+        return chars;
+    }
+
+    public MyString(char[] chars) {
+        this.chars = chars;
+    }
+
+    public static MyString add(MyString first, MyString second) {
+        char[] result = new char[first.chars.length + second.chars.length];
+        for (int i = 0; i < first.chars.length; i++) {
+            result[i] = first.chars[i];
+        }
+        int count = 0;
+        for (int i = first.chars.length; i < result.length; i++) {
+            result[i] = second.chars[count];
+            count++;
+        }
+        return new MyString(result);
+    }
+
+    public static MyString minus(MyString first, int substring) throws Exception {
+
+        if (first.chars.length < substring) {
+            throw new Exception("Номер подстроки не может быть больше строки");
+        }
+        char[] result = new char[first.chars.length - substring];
+        for (int i = 0; i < first.chars.length - substring; i++) {
+            result[i] = first.chars[i];
+        }
+        return new MyString(result);
+
+    }
+
+    public static boolean theSame(MyString obj1, MyString obj2) {
+        if (obj1.chars.length != obj2.chars.length) {
+            return false;
+        }
+        boolean flag = true;
+        for (int i = 0; i < obj1.chars.length; i++) {
+            if (obj1.chars[i] != obj2.chars[i]) {
+                flag = false;
+            }
+        }
+        return flag;
+
+    }
+
+    public static String myStringToString(MyString ob) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ob.chars.length; i++) {
+            sb = sb.append(ob.chars[i]);
+        }
+        return sb.toString();
+    }
 }
+
--- src/Task5/task4/task4.java
+++ src/Task5/task4/task4.java
@@ -1,6 +1,29 @@
 package Task5.task4;
 
-public class task4 {
+import static Task5.task4.MyString.*;
 
+public class task4 {
+    public static void main(String[] args) throws Exception {
+        char[] chars = new char[]{'a', 'b', 'd'};
+        char[] chars2 = new char[]{'a', 'b', 'v', 'q'};
+
+        MyString a = new MyString(chars);
+        MyString b = new MyString(chars2);
+        MyString q = add(a, b);
+
+        for (int i = 0; i < q.getChars().length; i++) {
+
+            System.out.println(q.getChars()[i]);
+        }
+
+        System.out.println(theSame(a, b));
+        a = MyString.minus(a, 1);
+        for (int i = 0; i < a.getChars().length; i++) {
+
+            System.out.println(a.getChars()[i]);
+        }
+        System.out.println(myStringToString(a));
+
+    }
 
 }
