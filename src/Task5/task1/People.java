ab99762f0e5aab037c0af546a85f9d32b2d16322
kolyandia1998 <93881367+kolyandia1998@users.noreply.github.com>
2022-07-26 14:56:14 +0300

    test

--- src/Task5/task1/People.java
+++ src/Task5/task1/People.java
@@ -10,25 +10,25 @@ import java.util.regex.Pattern;
 public class People {
     private String surname;
     private String name;
-    private String middle_name;
-    private LocalDate birth_day = LocalDate.of(1900, 1, 1);
+    private String middleName;
+    private LocalDate birthDay = LocalDate.of(1900, 1, 1);
 
 
     public void setBirth_day(int year, int month, int dayOfMonth) throws Exception {
         try {
-            this.birth_day = LocalDate.of(year, Month.of(month), dayOfMonth);
+            this.birthDay = LocalDate.of(year, Month.of(month), dayOfMonth);
         } catch (DateTimeException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public LocalDate getBirth_day() {
-        return birth_day;
+        return birthDay;
     }
     private String reg = "^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$";
 
     public int getAge() {
-        return LocalDate.now().compareTo(this.birth_day);
+        return LocalDate.now().compareTo(this.birthDay);
     }
 
     public void setName(String name) throws NameException {
@@ -59,11 +59,11 @@ public class People {
         if (middle_name == null || !middle_name.matches(reg)) {
             throw new NameException("Фамилия должна состоять из букв");
         }
-        this.middle_name = middle_name;
+        this.middleName = middle_name;
     }
 
     public String getMiddle_name() {
-        return middle_name;
+        return middleName;
     }
 
 
