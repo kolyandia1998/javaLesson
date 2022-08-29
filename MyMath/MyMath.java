cfe5ea4ed77d892b8a330376091c79db7a35f0ff
kolyandia1998 <93881367+kolyandia1998@users.noreply.github.com>
2022-08-29 10:33:51 +0300

    tst
    test

---
+++ MyMath/MyMath.java
@@ -0,0 +1,21 @@
+package MyMath;
+
+public class MyMath {
+    public static int factorial (int value) {
+           if (value == 1)
+           {return  1;}
+           return  factorial(value -1) * value;
+    }
+
+    public static int Exponentiation (int value, int degree) {
+        if (degree == 0)
+         return 1;
+        else if (degree==1)
+         return value;
+            int res = 1;
+        for (int i=0 ; i < degree; i++) {
+             res = value * res;
+        }
+          return res;
+    }
+}
