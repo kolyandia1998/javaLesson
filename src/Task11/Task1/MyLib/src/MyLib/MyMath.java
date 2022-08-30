24b1aacefa1638cf2096052d6c3718b69aa8c869
kolyandia1998 <93881367+kolyandia1998@users.noreply.github.com>
2022-08-30 13:16:07 +0300

    Task11,full

--- src/Task11/Task1/MyLib/src/MyLib/MyMath.java
+++ src/Task11/Task1/MyLib/src/MyLib/MyMath.java
@@ -1,19 +1,20 @@
 package MyLib;
 
 public class MyMath {
-    public static int factorial (int value) {
-        if (value == 1)
-        {return  1;}
-        return  factorial(value -1) * value;
+    public static int factorial(int value) {
+        if (value == 1) {
+            return 1;
+        }
+        return factorial(value - 1) * value;
     }
 
-    public static int Exponentiation (int value, int degree) {
+    public static int Exponentiation(int value, int degree) {
         if (degree == 0)
             return 1;
-        else if (degree==1)
+        else if (degree == 1)
             return value;
         int res = 1;
-        for (int i=0 ; i < degree; i++) {
+        for (int i = 0; i < degree; i++) {
             res = value * res;
         }
         return res;
